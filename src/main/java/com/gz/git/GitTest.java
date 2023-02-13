000476f7b8c7bf7176df266f7cc394d46857a5f8
geogre <23473042@qq.com>
2023-02-13 16:36:56 +0800

    git-Test111 commit

--- src/main/java/com/gz/git/GitTest.java
+++ src/main/java/com/gz/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("git-Test");
+        System.out.println("git-Test1111");
     }
 }
